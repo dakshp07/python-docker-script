eed91791f6d818ffb1dbc94ddc9317173803d3ad
dakshp07 <dpaleria@gmail.com>
2021-08-08 20:01:58 +0530

    Updated Script

--- main_csv.py
+++ main_csv.py
@@ -1,3 +1,4 @@
+# Author: Daksh Paleria
 # The default path for scanning, saving csv is the root folder where you are running this script.
 # The default name of csv is result.csv
 # If you want to overwrite the default path to scan path to save csv, csv name then run the script in following manner:
@@ -25,6 +26,8 @@ import pandas as pd
 import cv2
 from PIL import Image
 import colorama
+import shutil
+import numpy as np
 from colorama import Fore
 
 # Function to get the directories name, file path, root
@@ -65,6 +68,20 @@ else:
     directory=sys.argv[1]
     directory_json=sys.argv[2]
     file_csv_name=sys.argv[3]
+
+# Creating directories for 3D Models
+for file in os.scandir(directory+"/vcmodels"):
+    if(file.name[8:9]=="e"):
+        path_vcmodel_one_arti=os.path.join(directory+"/vcmodels", file.name[0:7]+"_"+file.name[8:9])
+        os.makedirs(path_vcmodel_one_arti, exist_ok=True)
+        if(file.name[0:9]==path_vcmodel_one_arti[-9:]):
+            shutil.move(directory+"/vcmodels/"+file.name,path_vcmodel_one_arti)
+    else:
+        path_vcmodel_one_arti=os.path.join(directory+"/vcmodels", file.name[0:7])
+        os.makedirs(path_vcmodel_one_arti, exist_ok=True)
+        if(file.name[0:7]==path_vcmodel_one_arti[-7:]):
+            shutil.move(directory+"/vcmodels/"+file.name,path_vcmodel_one_arti)
+
 #eps = get_filepaths(directory+"/eps")
 lineart = get_filepaths(directory+"/lineart")
 long_translit = get_filepaths(directory+"/long_translit")
@@ -75,11 +92,13 @@ svg = get_filepaths(directory+"/svg")
 tn_lineart = get_filepaths(directory+"/tn_lineart")
 tn_photo = get_filepaths(directory+"/tn_photo")
 vcmodels = get_filepaths(directory+"/vcmodels")
+rti=get_filepaths(directory+"/rti")
 mode_to_bpp = {'1':1, 'L':8, 'P':8, 'RGB':24, 'RGBA':32, 'CMYK':32, 'YCbCr':24, 'I':32, 'F':32}
 
 print("Fetching File Names....")
 #Storing the data and dumping it in .json file
 file_name=[]
+file_name_vc=[]
 #for i in range(len(eps[0])):
     #item=eps[0][i]
     #file_name.append(item)
@@ -119,12 +138,25 @@ for i in range(len(tn_photo[0])):
 
 for i in range(len(vcmodels[0])):
     item=vcmodels[0][i]
+    file_name_vc.append(item)
+    file_name_vc_split=np.array_split(file_name_vc,(len(vcmodels[0])/2))
+print(file_name_vc_split[1][1][-3:])
+
+for i in range(len(rti[0])):
+    item=rti[0][i]
     file_name.append(item)
+
+for i in range(len(vcmodels[0])//2):
+    if(file_name_vc_split[i][0][-3:]=="jpg"):
+        file_name.append(file_name_vc_split[i][0])
+    elif (file_name_vc_split[i][1][-3:]=="jpg"):
+        file_name.append(file_name_vc_split[i][1])
 
 print("Fetching File names...."+Fore.GREEN+"done \n")
 
 print(Fore.WHITE+"Fetching Folder name....")
 folder_name=[]
+folder_name_vc=[]
 #for i in range(len(eps[2])):
     #item=eps[2][i][len(directory):]
     #folder_name.append(item)
@@ -164,10 +196,23 @@ for i in range(len(tn_photo[2])):
 
 for i in range(len(vcmodels[2])):
     item=vcmodels[2][i][len(directory):]
+    folder_name_vc.append(item)
+    folder_name_vc_split=np.array_split(folder_name_vc, (len(vcmodels[0])/2))
+
+for i in range(len(rti[2])):
+    item=rti[2][i][len(directory):]
     folder_name.append(item)
+
+for i in range(len(vcmodels[2])//2):
+    if(file_name_vc_split[i][0][-3:]=="jpg"):
+        folder_name.append(folder_name_vc_split[i][0])
+    elif (file_name_vc_split[i][1][-3:]=="jpg"):
+        folder_name.append(folder_name_vc_split[i][1])
+
 
 print("Fecthing Folder Name...."+Fore.GREEN+"done \n")
 artifact_id=[]
+artifact_id_vc=[]
 print(Fore.WHITE+"Fecthing Artifact ID....")
 
 #for i in range(len(eps[0])):
@@ -209,10 +254,30 @@ for i in range(len(tn_photo[0])):
 
 for i in range(len(vcmodels[0])):
     item=vcmodels[2][i][len(directory)+11:]
-    artifact_id.append(item.strip("0"))
+    if((vcmodels[2][i][len(directory)+11:])[-1:]=="e" or (vcmodels[2][i][len(directory)+11:])[-1:]=="a" or (vcmodels[2][i][len(directory)+11:])[-1:]=="b"):
+        artifact_id_vc.append(item[0:6].strip("0"))
+        artifact_id_vc_split=np.array_split(artifact_id_vc,(len(vcmodels[0])/2))
+    else:
+        artifact_id_vc.append(item.strip("0"))
+        artifact_id_vc_split=np.array_split(artifact_id_vc,(len(vcmodels[0])/2))
+
+for i in range(len(rti[0])):
+    if(rti[2][i][-1]=="o" or rti[2][i][-1]=="r"):
+        item=rti[2][i][len(directory)+6:-2]
+        artifact_id.append(item.strip("0"))
+    else:
+        item=rti[2][i][len(directory)+6:-3]
+        artifact_id.append(item.strip("0"))
+
+for i in range(len(vcmodels[0])//2):
+    if(file_name_vc_split[i][0][-3:]=="jpg"):
+        artifact_id.append(artifact_id_vc_split[i][0])
+    elif (file_name_vc_split[i][1][-3:]=="jpg"):
+        artifact_id.append(artifact_id_vc_split[i][1])
 
 print("Fecthing Artifact ID...."+Fore.GREEN+"done \n")
 image_type=[]
+image_type_vc=[]
 print(Fore.WHITE+"Fecthing Image Type....")
 
 #for i in range(len(eps[0])):
@@ -227,7 +292,7 @@ for i in range(len(lineart[0])):
     image_type.append(item)
 
 for i in range(len(long_translit[0])):
-    item="long translit"
+    item="long_translit"
     image_type.append(item)
 
 for i in range(len(pdf[0])):
@@ -253,25 +318,87 @@ for i in range(len(svg[0])):
     image_type.append(item)
 
 for i in range(len(tn_lineart[0])):
-    item="thumb lineart"
+    item="thumb_lineart"
     image_type.append(item)
 
 for i in range(len(tn_photo[0])):
-    item="thumb photo"
+    item="thumb_photo"
     image_type.append(item)
 
 for i in range(len(vcmodels[0])):
-    item="3D models"
-    image_type.append(item)
+    if (vcmodels[2][i][-1]=="e"):
+        item="3D_model_e"
+        image_type_vc.append(item)
+        image_type_vc_split=np.array_split(image_type_vc,(len(vcmodels[0])/2))
+    elif (vcmodels[2][i][-1]=="a"):
+        item="3D_model_a"
+        image_type_vc.append(item)
+        image_type_vc_split=np.array_split(image_type_vc,(len(vcmodels[0])/2))
+    elif (vcmodels[2][i][-1]=="b"):
+        item="3D_model_b"
+        image_type_vc.append(item)
+        image_type_vc_split=np.array_split(image_type_vc,(len(vcmodels[0])/2))
+    else:
+        item="3D_model"
+        image_type_vc.append(item)
+        image_type_vc_split=np.array_split(image_type_vc,(len(vcmodels[0])/2))
+
+for i in range(len(rti[0])):
+    if(rti[2][i][-1]=="o"):
+        item="rti_observe"
+        image_type.append(item)
+    elif(rti[2][i][-2]=='l'):
+        item="rti_left_edge"
+        image_type.append(item)
+    elif(rti[2][i][-2]=='b'):
+        item="rti_bottom_edge"
+        image_type.append(item)
+    elif(rti[2][i][-1]=='r'):
+        item="rti_reverse"
+        image_type.append(item)
+    elif(rti[2][i][-2]=='t'):
+        item="rti_top_edge"
+        image_type.append(item)
+    elif(rti[2][i][-2]=='r'):
+        item="rti_right_edge"
+        image_type.append(item)
+    elif(rti[2][i][-1]=='a'):
+        item="rti_face_a"
+        image_type.append(item)
+    elif(rti[2][i][-1]=='b'):
+        item="rti_face_b"
+        image_type.append(item)
+    elif(rti[2][i][-1]=='c'):
+        item="rti_face_c"
+        image_type.append(item)
+    elif(rti[2][i][-1]=='d'):
+        item="rti_face_d"
+        image_type.append(item)
+##print(rti[2][i][-2])
+    
+for i in range(len(vcmodels[0])//2):
+    if(file_name_vc_split[i][0][-3:]=="jpg"):
+        image_type.append(image_type_vc_split[i][0])
+    elif (file_name_vc_split[i][1][-3:]=="jpg"):
+        image_type.append(image_type_vc_split[i][1])
 
 print("Fetching Image Type...."+Fore.GREEN+"done \n")
 height=[]
+height_vc=[]
 width=[]
+width_vc=[]
 rgb=[]
+rgb_vc=[]
 bit=[]
+bits_vc=[]
 ppi=[]
+ppi_vc=[]
 size_bytes=[]
+size_bytes_vc=[]
 pixels=[]
+pixels_vc=[]
+format=[]
+format_vc=[]
 print(Fore.WHITE+"Fetching Height, Width, Size, PPI, Bits....")
 #for i in range(len(eps[0])):
     #item=eps[0][i]
@@ -294,6 +421,7 @@ for i in range(len(lineart[0])):
     size_bytes.append(size_value)
     pixels_value=w*h
     pixels.append(pixels_value)
+    format.append(lineart[1][i])
 
 for i in range(len(long_translit[0])):
     item=long_translit[0][i]
@@ -312,6 +440,7 @@ for i in range(len(long_translit[0])):
     size_bytes.append(size_value)
     pixels_value=w*h
     pixels.append(pixels_value)
+    format.append(long_translit[1][i])
 
 for i in range(len(pdf[0])):
     item=pdf[0][i]
@@ -322,6 +451,7 @@ for i in range(len(pdf[0])):
     ppi.append("")
     size_bytes.append("")
     pixels.append("")
+    format.append(pdf[1][i])
 
 for i in range(len(photo[0])):
     item=photo[0][i]
@@ -340,6 +470,7 @@ for i in range(len(photo[0])):
     size_bytes.append(size_value)
     pixels_value=w*h
     pixels.append(pixels_value)
+    format.append(photo[1][i])
 
 for i in range(len(ptm[0])):
     if(ptm[2][i][-1]=="o" or ptm[2][i][-1]=="r"):
@@ -359,6 +490,7 @@ for i in range(len(ptm[0])):
         size_bytes.append(size_value)
         pixels_value=w*h
         pixels.append(pixels_value)
+        format.append(ptm[1][i])
 
 for i in range(len(svg[0])):
     item=svg[0][i]
@@ -369,6 +501,7 @@ for i in range(len(svg[0])):
     ppi.append("")
     size_bytes.append("")
     pixels.append("")
+    format.append(svg[1][i])
 
 for i in range(len(tn_lineart[0])):
     item=tn_lineart[0][i]
@@ -387,6 +520,7 @@ for i in range(len(tn_lineart[0])):
     size_bytes.append(size_value)
     pixels_value=w*h
     pixels.append(pixels_value)
+    format.append(tn_lineart[1][i])
 
 for i in range(len(tn_photo[0])):
     item=tn_photo[0][i]
@@ -405,20 +539,71 @@ for i in range(len(tn_photo[0])):
     size_bytes.append(size_value)
     pixels_value=w*h
     pixels.append(pixels_value)
+    format.append(tn_photo[1][i])
 
 for i in range(len(vcmodels[0])):
     item=vcmodels[0][i]
-    height.append("")
-    width.append("")
-    rgb.append("")
-    bit.append("")
-    ppi.append("")
-    size_bytes.append("")
-    pixels.append("")
+    height_vc.append("")
+    height_vc_split=np.array_split(height_vc,(len(vcmodels[0])/2))
+    width_vc.append("")
+    width_vc_split=np.array_split(width_vc,(len(vcmodels[0])/2))
+    rgb_vc.append("")
+    rgb_vc_split=np.array_split(rgb_vc,(len(vcmodels[0])/2))
+    bits_vc.append("")
+    bits_vc_split=np.array_split(bits_vc,(len(vcmodels[0])/2))
+    ppi_vc.append("")
+    ppi_vc_split=np.array_split(ppi_vc,(len(vcmodels[0])/2))
+    size_bytes_vc.append("")
+    size_bytes_vc_split=np.array_split(size_bytes_vc,(len(vcmodels[0])/2))
+    pixels_vc.append("")
+    pixels_vc_split=np.array_split(pixels_vc,(len(vcmodels[0])/2))
+    format_vc.append(vcmodels[1][i])
+    format_vc_split=np.array_split(format_vc,(len(vcmodels[0])/2))
+
+for i in range(len(rti[0])):
+    item=rti[0][i]
+    im=cv2.imread(rti[2][i]+"/"+item)
+    h,w,c=im.shape
+    height.append(h)
+    width.append(w)
+    image=Image.open(rti[2][i]+"/"+item)
+    colors=image.getpixel((320,420))
+    rgb.append(colors)
+    bpp = mode_to_bpp[image.mode]
+    bit.append(bpp)
+    ppi_value=w/(w*0.01)
+    ppi.append(ppi_value)
+    size_value=str((os.stat(rti[2][i]+"/"+item).st_size)*0.000001)
+    size_bytes.append(size_value)
+    pixels_value=w*h
+    pixels.append(pixels_value)
+    format.append(rti[1][i])
+    
+for i in range(len(vcmodels[0])//2): 
+    if(file_name_vc_split[i][0][-3:]=="jpg"):
+        format.append(format_vc_split[i][0])
+        height.append(height_vc_split[i][0])
+        width.append(width_vc_split[i][0])
+        rgb.append(rgb_vc_split[i][0])
+        ppi.append(ppi_vc_split[i][0])
+        size_bytes.append(size_bytes_vc_split[i][0])
+        pixels.append(pixels_vc_split[i][0])
+        bit.append(bits_vc_split[i][0])
+    elif (file_name_vc_split[i][1][-3:]=="jpg"):
+        format.append(format_vc_split[i][1])
+        height.append(height_vc_split[i][1])
+        width.append(width_vc_split[i][1])
+        rgb.append(rgb_vc_split[i][1])
+        ppi.append(ppi_vc_split[i][1])
+        size_bytes.append(size_bytes_vc_split[i][1])
+        pixels.append(pixels_vc_split[i][1])
+        bit.append(bits_vc_split[i][1])
 
 print("Fetching Height, Width, Size, PPI, Bits...."+Fore.GREEN+"done \n")
 modified_date=[]
 created_date=[]
+modified_date_vc=[]
+created_date_vc=[]
 print(Fore.WHITE+"Fecthing Dates....")
 #for i in range(len(eps[0])):
     #create=time.ctime(os.path.getctime(eps[2][i]+"/"+eps[0][i]))
@@ -478,11 +663,32 @@ for i in range(len(tn_photo[0])):
 for i in range(len(vcmodels[0])):
     create=datetime.datetime.fromtimestamp(os.path.getctime(vcmodels[2][i]+"/"+vcmodels[0][i]))
     modify=datetime.datetime.fromtimestamp(os.path.getmtime(vcmodels[2][i]+"/"+vcmodels[0][i]))
+    create_date=create.strftime('%Y-%m-%d %H:%M:%S.%f')
+    modify_date=modify.strftime('%Y-%m-%d %H:%M:%S.%f')
+    created_date_vc.append(create_date)
+    created_date_vc_split=np.array_split(created_date_vc,(len(vcmodels[0])/2))
+    modified_date_vc.append(modify_date)
+    modified_date_vc_split=np.array_split(modified_date_vc,(len(vcmodels[0])/2))
+
+for i in range(len(rti[0])):
+    create=datetime.datetime.fromtimestamp(os.path.getctime(rti[2][i]+"/"+rti[0][i]))
+    modify=datetime.datetime.fromtimestamp(os.path.getmtime(rti[2][i]+"/"+rti[0][i]))
     created_date.append(create)
     modified_date.append(modify)
+    
+for i in range(len(vcmodels[0])//2):
+    if(file_name_vc_split[i][0][-3:]=="jpg"):
+        created_date.append(created_date_vc_split[i][0])
+        modified_date.append(modified_date_vc_split[i][0])
+    elif (file_name_vc_split[i][1][-3:]=="jpg"):
+        modified_date.append(modified_date_vc_split[i][1])
+        created_date.append(created_date_vc_split[i][1])
+    #created_date.append(created_date_vc_split[i])
+    #modified_date.append(modified_date_vc_split[i])
 
 print("Fetching Dates...."+Fore.GREEN+"done \n")
-dict={"file_name":file_name, "folder_name":folder_name, "artifact_id":artifact_id, "image_type": image_type, "creation_date":created_date, "modify_date":modified_date, "height":height, "width":width, "rgb":rgb, "bit":bit, "ppi":ppi, "size_mb":size_bytes, "size_pixels":pixels}
+dict={"file_name":file_name, "folder_name":folder_name, "artifact_id":artifact_id, "image_type": image_type, "creation_date":created_date, "modify_date":modified_date, "height":height, "width":width, "rgb":rgb, "bit":bit, "ppi":ppi, "size_mb":size_bytes, "size_pixels":pixels, "format":format}
+##print(dict)
 dataframe=pd.DataFrame(dict)
 dataframe.to_csv(directory_json+"/"+file_csv_name, index=False)
 print(Fore.WHITE+"Successfully Dumped at "+Fore.GREEN+directory_json+"/"+file_csv_name)
